9a8bd1481d37c5458c512cd03053862c0146ea4c
Aluizio Junior <986053902z@gmail.com>
2024-07-04 10:51:37 -0300

    Add files via upload

---
+++ texte form/cadastro.php
@@ -0,0 +1,15 @@
+<?php
+
+include("./conexao.php");
+
+$nome_comp = $_POST["nome_comp"];
+$idade = $_POST["idade"];
+$profissão = $_POST["profissão"];
+    
+echo $nome_comp;
+echo $idade;
+echo $profissão;
+?>
+
+
+
